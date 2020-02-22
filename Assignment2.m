659aefeee6319d251c30374b4e121f1f1832eb1f
Adam Heffernan <adamheffernan@live.com>
2020-02-22 13:41:45 -0500

    Need to finish commenting 1-B - 2D then this baby's done

--- Assignment2.m
+++ Assignment2.m
@@ -1,27 +1,65 @@
-%% Script for Assignment 2 ELEC 4700 Completed on 2/19/2020. 
-%  Adam Heffernan 100977570
+%% Assignment 2 ELEC 4700 Finite Difference Method
+% Adam Heffernan 100977570
+% Completed February 22nd 2020
 
-%%This is Part 1A
+%%  
+% In this section we model two simple cases. a 1D and 2D case using
+% Laplaces finite difference method in Matrix form as to make it easier to
+% add boxes into the simulation. The dimmensions given in the problem
+% states a 3/2 Length v. Width, as such I used 30 and 20 as my mesh sizes
+% for all problems solved in the scripts below. The Voltage at the boundary
+% conditions is set to 1V for all cases modeled below. 
+%
+%
+%
+%%
+%% Part 1A
 Assignment2_Part1(1)
-
-
-%%This is Part 1B
+%%
+% Figure 1 Above shows the 1D potential case where $X = V_0$ while $X = 0$ 
+% and the potential as we approach all 3 other sides goes to 0. That is $X = 0$
+% at $X = length$, $Y=0$ while $Y = 0$ and $Y = Width$. In this case the
+% voltage is constant in the Y direction at 0V and rises linearly to 1V at
+% between $X = 0$ and $X = length$
+%
+%
+%%
+%% Part 1B
 Assignment2_Part1(2)
-
-
-%%This is Part 2A
+%%
+% Figure 2 and 3 Above shows the 1D potential case where $X = V_0$ while $X = 0$ 
+% and the potential as we approach all 3 other sides goes to 0. That is $X = 0$
+% at $X = length$, $Y=0$ while $Y = 0$ and $Y = Width$. In this case the
+% voltage is constant in the Y direction at 0V and rises linearly to 1V at
+% between $X = 0$ and $X = length$
+%
+%%
+%% Part 2A
 Assignment2_Part2(1)
-
-
-%%This is Part 2B
+%%
+% Figure 4,5,6,7
+%
+%%
+%% Part 2B Varrying mesh sizes 
 Assignment2_Part2(2)
-
-
-%%This is Part 2C
+%%
+% Figure 8
+%
+%%
+%% Part 2C-1 Varrying Box Width
 Assignment2_Part2(3)
-
-
-%%This is Part 2D
+%%
+% Figure 9
+%
+%%
+%% Part 2C-2 Varrying Box Height
 Assignment2_Part2(4)
-
-%This is the end of the script
+%%
+% Figure 10
+%
+%%
+%% Part 2D Varrying the conductivity density map 
+Assignment2_Part2(5)
+%%
+% Figure 11
+%
--- src/Assignment2_Part1.m
+++ src/Assignment2_Part1.m
@@ -1,188 +1,169 @@
 function Assignment2_Part1(part)
 %% Function for Part 1 of Assignment 2 4700. Adam Heffernan 100977570
 %Input a 1 to simulate Part 1A or a 2 to run part 1B
- 
+
 %Variables
 len=30;
 width=20;
-v_0 = 1; 
 iter=100;
 sigma_map=ones(width,len);
 F=zeros(1,len*width);
 G=zeros(len*width,len*width);
 %% Part 1A
 if part == 1
-for i=1:len
-    for j=1:width
-        Updater=j+(i-1)*width;
-        
-        if(i==1)%left
-           
-            G(Updater,Updater)=1;
-            F(Updater)=1;
+    for i=1:len
+        for j=1:width
+            Updater=j+(i-1)*width;
             
-        elseif(i==len)%right
-            
-            G(Updater,Updater)=1;
-            F(Updater)=0;
-            
-        elseif(j==1)%bottom
- 
-            G(Updater,Updater)=1;
-            F(Updater)=0;
-            
-        elseif(j==width)%top
-
-            G(Updater,Updater)=1;
-            F(Updater)=0;
-            
-        else%middle
-            Nyu=j+1+(i-1)*width;
-            Nyd=j-1+(i-1)*width;
-            Nxu=j+(i)*width;
-            Nxd=j+(i-1-1)*width;
-
-            Ryu=(sigma_map(j,i)+sigma_map(j+1,i))/2.0;
-            Ryd=(sigma_map(j,i)+sigma_map(j-1,i))/2.0;
-            Rxu=(sigma_map(j,i)+sigma_map(j,i+1))/2.0;
-            Rxd=(sigma_map(j,i)+sigma_map(j,i-1))/2.0;
-            
-            
-            G(Updater,Updater)=-(Ryu+Ryd+Rxu+Rxd);
-            G(Updater,Nyu)=Ryu;
-            G(Updater,Nyd)=Ryd;
-            G(Updater,Nxu)=Rxu;
-            G(Updater,Nxd)=Rxd;
-            
+            if(i==1)%left
+                
+                G(Updater,Updater)=1;
+                F(Updater)=1;
+                
+            elseif(i==len)%right
+                
+                G(Updater,Updater)=1;
+                F(Updater)=0;
+                
+            elseif(j==1)%bottom
+                
+                G(Updater,Updater)=1;
+                F(Updater)=0;
+                
+            elseif(j==width)%top
+                
+                G(Updater,Updater)=1;
+                F(Updater)=0;
+                
+            else%middle
+                Nyu=j+1+(i-1)*width;
+                Nyd=j-1+(i-1)*width;
+                Nxu=j+(i)*width;
+                Nxd=j+(i-1-1)*width;
+                
+                Ty_Over=(sigma_map(j,i)+sigma_map(j+1,i))/2.0;
+                Ty_Under=(sigma_map(j,i)+sigma_map(j-1,i))/2.0;
+                Tx_Over=(sigma_map(j,i)+sigma_map(j,i+1))/2.0;
+                Tx_Under=(sigma_map(j,i)+sigma_map(j,i-1))/2.0;
+                
+                
+                G(Updater,Updater)=-(Ty_Over+Ty_Under+Tx_Over+Tx_Under);
+                G(Updater,Nyu)=Ty_Over;
+                G(Updater,Nyd)=Ty_Under;
+                G(Updater,Nxu)=Tx_Over;
+                G(Updater,Nxd)=Tx_Under;
+                
+            end  
         end
-            
-            
-            
-        
     end
-end
-
-F=F';
-
-V=G\F;
-V_1D = zeros(width,len);
-for i=1:len
-    for j=1:width
-
-
-        Updater=j+(i-1)*width;
-
-        V_1D(j,i)=V(Updater);
-
-    end
-end
-
-figure(1)
-surf(V_1D);
-title('Figure 1 Voltage Map for Finite Difference Method Single Boundary' );
-ylabel('y position');
-xlabel('x position');
-c=colorbar;
-c.Label.String = 'V (V)';
-view(0,90);
-
-%% Part 1 B 
-elseif part == 2 
-for i=1:len
-    for j=1:width
-        Updater=j+(i-1)*width;
-        
-        if(i==1)%left
-           
-            G(Updater,Updater)=1;
-            F(Updater)=1;
-            
-        elseif(i==len)%right
-            
-            G(Updater,Updater)=1;
-            F(Updater)=1;
-            
-        elseif(j==1)%bottom
- 
-            G(Updater,Updater)=1;
-            F(Updater)=0;
-            
-        elseif(j==width)%top
-
-            G(Updater,Updater)=1;
-            F(Updater)=0;
-            
-        else%middle
-            Nyu=j+1+(i-1)*width;
-            Nyd=j-1+(i-1)*width;
-            Nxu=j+(i)*width;
-            Nxd=j+(i-1-1)*width;
-
-            Ryu=(sigma_map(j,i)+sigma_map(j+1,i))/2.0;
-            Ryd=(sigma_map(j,i)+sigma_map(j-1,i))/2.0;
-            Rxu=(sigma_map(j,i)+sigma_map(j,i+1))/2.0;
-            Rxd=(sigma_map(j,i)+sigma_map(j,i-1))/2.0;
-            
-            
-            G(Updater,Updater)=-(Ryu+Ryd+Rxu+Rxd);
-            G(Updater,Nyu)=Ryu;
-            G(Updater,Nyd)=Ryd;
-            G(Updater,Nxu)=Rxu;
-            G(Updater,Nxd)=Rxd;
-            
-        end
-            
-            
-            
-        
-    end
-end
-
-F=F';
-
-V=G\F;
-V_2D = zeros(width,len);
-for i=1:len
-    for j=1:width
-
-
-        Updater=j+(i-1)*width;
-
-        V_2D(j,i)=V(Updater);
-
-    end
-end    
-figure(2)
-surf(V_2D);
-title(' Figure 1 Voltage Map for Finite Difference Method 2 raised potentials');
-ylabel('y position');
-xlabel('x position');
-c=colorbar;
-c.Label.String = 'Volts';
-view(0,90);
-
-new_width=width-1;
-new_len=(len-1)/2.0;
-y_coordinates=0:1:new_width;
-x_coordinates=-new_len:1:new_len;
-[Xmesh,Ymesh]=meshgrid(x_coordinates,y_coordinates);
-V_analytical=zeros(length(y_coordinates),length(x_coordinates));
-
-
-for n=1:2:iter
     
-V_analytical=V_analytical+(4.*v_0./pi).*(cosh(n.*pi.*Xmesh./new_width).*sin(n.*pi.*Ymesh./new_width)./(n.*cosh(n.*pi.*new_len./new_width)));
+    F=F';
+    
+    V=G\F;
+    V_1D = zeros(width,len);
+    for i=1:len
+        for j=1:width
+            Updater=j+(i-1)*width;
+            V_1D(j,i)=V(Updater);  
+        end
+    end
+    
+    figure(1)
+    surf(V_1D,'EdgeColor','none','FaceColor','interp');
+    title('Finite Difference Method for Single Boundary Conditon' );
+    ylabel('Y');
+    xlabel('X');
+    c=colorbar;
+    c.Label.String = 'Volts';
+    view(0,90);
+    
+    %% Part 1 B
+elseif part == 2
+    for i=1:len
+        for j=1:width
+            Updater=j+(i-1)*width;
+            
+            if(i==1)%left
+                
+                G(Updater,Updater)=1;
+                F(Updater)=1;
+                
+            elseif(i==len)%right
+                
+                G(Updater,Updater)=1;
+                F(Updater)=1;
+                
+            elseif(j==1)%bottom
+                
+                G(Updater,Updater)=1;
+                F(Updater)=0;
+                
+            elseif(j==width)%top
+                
+                G(Updater,Updater)=1;
+                F(Updater)=0;
+                
+            else%middle
+                Nyu=j+1+(i-1)*width;
+                Nyd=j-1+(i-1)*width;
+                Nxu=j+(i)*width;
+                Nxd=j+(i-1-1)*width;
+                
+                Ty_Over=(sigma_map(j,i)+sigma_map(j+1,i))/2.0;
+                Ty_Under=(sigma_map(j,i)+sigma_map(j-1,i))/2.0;
+                Tx_Over=(sigma_map(j,i)+sigma_map(j,i+1))/2.0;
+                Tx_Under=(sigma_map(j,i)+sigma_map(j,i-1))/2.0;
+   
+                G(Updater,Updater)=-(Ty_Over+Ty_Under+Tx_Over+Tx_Under);
+                G(Updater,Nyu)=Ty_Over;
+                G(Updater,Nyd)=Ty_Under;
+                G(Updater,Nxu)=Tx_Over;
+                G(Updater,Nxd)=Tx_Under;
+                
+            end
+        end
+    end
+    
+    F=F';
+    
+    V=G\F;
+    V_2D = zeros(width,len);
+    for i=1:len
+        for j=1:width
+            Updater=j+(i-1)*width;
+            V_2D(j,i)=V(Updater);
+        end
+    end
+    figure(2)
+    surf(V_2D,'EdgeColor','none','FaceColor','interp');
+    title('Finite Difference Method for Given Boundaries');
+    ylabel('Y');
+    xlabel('X');
+    c=colorbar;
+    c.Label.String = 'Volts';
+    view(0,90);
+    
+    trunked_width=width-1;
+    trunked_len=(len-1)/2.0;
+    y_coordinates=0:1:trunked_width;
+    x_coordinates=-trunked_len:1:trunked_len;
+    [Xmesh,Ymesh]=meshgrid(x_coordinates,y_coordinates);
+    V_analytical=zeros(length(y_coordinates),length(x_coordinates));
 
-end
-figure(3)
-surf(V_analytical)
-title('Figure 2 Voltage Map for Analytical Solution');
-ylabel('y position');
-xlabel('x position');
-c=colorbar;
-c.Label.String = 'Volts';
-view(0,90);
-else 
+    for n=1:2:iter
+        
+        V_analytical=V_analytical+(4.*1./pi).*(cosh(n.*pi.*Xmesh./trunked_width).*sin(n.*pi.*Ymesh./trunked_width)./(n.*cosh(n.*pi.*trunked_len./trunked_width)));
+        
+    end
+    figure(3)
+    surf(V_analytical,'EdgeColor','none','FaceColor','interp')
+    title('Analytical Solution of Laplaces Equation for Given Boundaries');
+    ylabel('Y');
+    xlabel('X');
+    c=colorbar;
+    c.Label.String = 'Volts';
+    view(0,90);
+else
     return
 end
 end
